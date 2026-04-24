270d795f851b42f7e6bdd50a0f11fcf97b776fbe
ZSCaiGou <11460269+upontime@user.noreply.gitee.com>
2026-04-24 15:16:07 +0800

    fix

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,11 @@
-import Koa from "koa";
+import koa from "koa";
+
+const app = new koa();
+
+app.use((ctx) => {
+  ctx.body = "hello world";
+});
+
+app.listen(3000, () => {
+  console.log("server is running at port 3000");
+});
